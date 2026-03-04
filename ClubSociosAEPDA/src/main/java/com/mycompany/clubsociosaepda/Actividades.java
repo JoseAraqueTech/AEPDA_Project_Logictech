5ecac2f5151736ac0990e290cd264cf2cdc22ecb
Enric Espinosa <enric.espinosa.aguayo@ieselcalamot.com>
2026-03-04 19:31:49 +0100

    He creado el menu y también un archivo con los metodos

--- ClubSociosAEPDA/src/main/java/com/mycompany/clubsociosaepda/Actividades.java
+++ ClubSociosAEPDA/src/main/java/com/mycompany/clubsociosaepda/Actividades.java
@@ -1,15 +1,13 @@
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-
 package com.mycompany.clubsociosaepda;
 
 /**
  *
- * @author josea
+ * @author enric
  */
-public class ClubSociosAEPDA {
-
-   
-    }
-
+public class Actividades {
+    
+}
---
+++ ClubSociosAEPDA/src/main/java/com/mycompany/clubsociosaepda/GestorSocios.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.clubsociosaepda;
+
+/**
+ *
+ * @author enric
+ */
+public class GestorSocios {
+    public void menu() {
+            System.out.println("Registro de usuario: ");
+            System.out.println("Control de cuotas y pagos: ");
+            System.out.println("Registros de gastos generales: ");
+            System.out.println("Gestion de baldas: ");
+            System.out.println("");
+            
+    
+}
+}
