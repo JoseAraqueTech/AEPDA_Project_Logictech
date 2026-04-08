7121961329696b85e8fc208244b051e155f2b79d
andres.bascuñan.oyarzo <andres.bascunan.oyarzo@ieselcalamot.com>
2026-04-08 23:39:35 +0200

    Docs: added full Javadoc to Activitat class

--- ClubSociosAEPDA/src/main/java/com/mycompany/clubsociosaepda/ClasesAEPDA/Activitat.java
+++ ClubSociosAEPDA/src/main/java/com/mycompany/clubsociosaepda/ClasesAEPDA/Activitat.java
@@ -7,10 +7,13 @@ package com.mycompany.clubsociosaepda.ClasesAEPDA;
 import java.time.LocalDate;
 import java.util.ArrayList;
 
-/**
- *
- * @author juan-
- */
+    /**
+     * Creates a new activity with the specified name and date.
+     *
+     * @param nom the activity name
+     * @param data the activity date as a LocalDate
+     */
+
 public class Activitat {
 
     private String nom;
@@ -22,21 +25,47 @@ public class Activitat {
         this.data = data;
         this.participants = new ArrayList<>();
     }
+    /**
+     * Gets the activity name.
+     *
+     * @return the activity name
+     */
 
     public String getNom() {
         return nom;
     }
+    /**
+     * Gets the activity date.
+     *
+     * @return the date of the activity
+     */
 
     public LocalDate getData() {
         return data;
     }
+        /**
+     * Sets the activity date.
+     *
+     * @param data the new date for the activity
+     */
+
     public void setData(LocalDate data){
        this.data = data;
     }
+    /**
+     * Gets the list of participants registered in the activity.
+     *
+     * @return a list of users participating in the activity
+     */
 
     public ArrayList<Usuari> getParticipants() {
         return participants;
     }
+    /**
+     * Adds a user as a participant in the activity.
+     *
+     * @param u the user to add
+     */
 
     public void afegirParticipant(Usuari u) {
         participants.add(u);
